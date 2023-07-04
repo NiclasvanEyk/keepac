d2dde859f16c817486af92c200fba7e1a50f5e35
Niclas van Eyk <niclas.eyk@gmail.com>
2023-07-04 19:17:06 +0200

    make the root command run `show`
    fixes #8

--- cmd/root.go
+++ cmd/root.go
@@ -18,9 +18,7 @@ examples and usage of using your application. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	RunE: showCmd.RunE,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
